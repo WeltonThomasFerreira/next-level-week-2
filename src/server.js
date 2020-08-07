f7eb89384e088e07f8568dfcc5df153d83769757
Thomas Ferreira <weltonthomasferreira@outlook.com>
2020-08-07 20:59:10 -0300

    install nodemon

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-import 'express';
+require('express')()
+    .get("/", (req, res) => {
+        return res.send("Hi from NLW")
+    })
+    .get("/study", (req, res) => {
+        return res.send("Study")
+     })
+    .listen(5500)
